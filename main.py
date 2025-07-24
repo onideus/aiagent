9cb95455898129957c41317db8f9af1138b302b3
Zach Martin <zach@zach-martin.com>
2025-07-24 14:52:52 -0400

    API call for gemini in place

--- main.py
+++ main.py
@@ -1,6 +1,14 @@
-def main():
-    print("Hello from aiagent!")
+import os
+from dotenv import load_dotenv
+from google import genai
 
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+
+def main():
+    client = genai.Client(api_key=api_key)
+    print(client.models.generate_content(model="gemini-2.0-flash-001",
+                                         contents="What's makes python a great language for llms? Limit your answer to roughly a paragraph in length.").text)
 
 if __name__ == "__main__":
     main()
